dbb542317c8c78dce5d47600b19bcd8f4b94fe22
Ewa Perzanowska <ewaperzanowska5@gmail.com>
2016-09-22 18:19:36 +0200

    zadanie 8.5 - instrukcje warunkowe

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,27 @@ var a = 3,
 	triangleArea = a*h/2;
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' +triangleArea);
+
+var a = 1,
+	b = 2,
+	value = (a*a) + (2*a*b) - (b*b);
+
+console.log('Wynik działania (a*a) + (2*a*b) - (b*b), gdy zmienna a jest równa ' + a + ', zaś zmienna b jest równa ' + b + ', wynosi ' +value)
+
+if (value > 0) {
+
+	console.log('Wynik dodatni');
+
+}	else if (value < 0 ) {
+
+	console.log('Wynik ujemny');
+
+}	else {
+
+	console.log('Wynik jest równy zero');
+}
+
+
+
+	
+
